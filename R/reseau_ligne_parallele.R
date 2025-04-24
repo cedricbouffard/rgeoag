e1b51a81dd3686a15bf2b422bf57a18beba5864b
cedricbouffard <80600509+cedricbouffard@users.noreply.github.com>
2025-04-24 08:18:04 -0400

    modification resea ligne parrallele

--- R/reseau_ligne_parallele.R
+++ R/reseau_ligne_parallele.R
@@ -1,38 +1,49 @@
-#' Créer un réseau de lignes parallèles orientées
+#' Créer un réseau de lignes parallèles orientées avec angle et coin dynamiques
 #'
-#' Cette fonction génère un réseau de lignes parallèles orientées selon un angle donné,
-#' espacées d'une largeur fixe, et couvrant une emprise fournie.
+#' Cette fonction génère un réseau de lignes parallèles orientées selon un angle donné ou calculé automatiquement,
+#' espacées d'une largeur fixe, et couvrant une emprise fournie. Permet de choisir le coin de départ.
 #'
-#' @param polygone Un objet `sf` de type POLYGON, MULTIPOLYGON ou `bbox` représentant la zone à couvrir
-#' @param angle Angle d’orientation des lignes en degrés (par rapport à l’axe des x, dans le sens antihoraire)
-#' @param largeur Espacement entre les lignes (en unités du système de coordonnées)
+#' @param polygone Objet `sf` (POLYGON ou MULTIPOLYGON)
+#' @param angle (optionnel) Angle en degrés. Si NULL, calculé automatiquement.
+#' @param largeur Espacement entre les lignes.
+#' @param coin Coin d’ancrage : `"SO"`, `"SE"`, `"NE"`, `"NO"` (par défaut `"SO"`)
 #'
-#' @return Un objet `sf` de type LINESTRING contenant le réseau de lignes découpé à l'emprise
+#' @return Un objet `sf` de lignes coupées à l'emprise du polygone
 #' @export
-#' @importFrom sf st_bbox st_as_sfc st_crs st_intersection st_sfc st_linestring st_geometry
-reseau_lignes_paralleles <- function(polygone, angle, largeur) {
-  stopifnot(inherits(polygone, "sf") || inherits(polygone, "sfc") || inherits(polygone, "bbox"))
+reseau_lignes_paralleles <- function(polygone, angle = NULL, largeur = 20, coin = "SO") {
+  stopifnot(inherits(polygone, "sf") || inherits(polygone, "sfc"))
 
-  emprise <- sf::st_as_sfc(sf::st_bbox(polygone))
-  crs <- sf::st_crs(emprise)
+  crs <- sf::st_crs(polygone)
+  polygone <- sf::st_union(polygone) |> sf::st_transform(4326)
 
-  bbox <- sf::st_bbox(emprise)
-  diag_len <- sqrt((bbox$xmax - bbox$xmin)^2 + (bbox$ymax - bbox$ymin)^2)
-
+  # Rectangle orienté minimal pour angle et coin
+  rect_info <- rectangle_oriente_min(polygone)
+  angle_calcule <- rect_info$angle
+  angle <- if (is.null(angle)) angle_calcule else angle
   angle_rad <- angle * pi / 180
+
+  # Création des lignes dans un repère tourné
+  bbox <- sf::st_bbox(polygone)
+  centre <- c(mean(c(bbox["xmin"], bbox["xmax"])), mean(c(bbox["ymin"], bbox["ymax"])))
+
   dx <- cos(angle_rad)
   dy <- sin(angle_rad)
-
   px <- -dy
   py <- dx
 
+  diag_len <- sqrt((bbox["xmax"] - bbox["xmin"])^2 + (bbox["ymax"] - bbox["ymin"])^2)
   n_lignes <- ceiling((2 * diag_len) / largeur)
-  lignes <- vector("list", length = n_lignes)
 
+  # Choix du coin d'origine pour le centrage
+  coins <- c("SO", "SE", "NE", "NO")
+  coords <- sf::st_coordinates(rect_info$rectangle)[1:4, 1:2]
+  coord_coin <- coords[match(coin, coins), ]
+
+  lignes <- vector("list", n_lignes)
   for (i in seq_len(n_lignes)) {
     offset <- (i - n_lignes / 2) * largeur
-    x0 <- (bbox$xmin + bbox$xmax) / 2 + offset * px
-    y0 <- (bbox$ymin + bbox$ymax) / 2 + offset * py
+    x0 <- coord_coin[1] + offset * px
+    y0 <- coord_coin[2] + offset * py
 
     p1 <- c(x0 - dx * diag_len, y0 - dy * diag_len)
     p2 <- c(x0 + dx * diag_len, y0 + dy * diag_len)
@@ -40,7 +51,10 @@ reseau_lignes_paralleles <- function(polygone, angle, largeur) {
     lignes[[i]] <- sf::st_linestring(rbind(p1, p2))
   }
 
-  reseau <- sf::st_sfc(lignes, crs = crs)
+  reseau <- sf::st_sfc(lignes, crs = 4326)
+  reseau <- sf::st_transform(reseau, crs)
+  polygone <- sf::st_transform(polygone, crs)
+
   reseau_clipped <- sf::st_intersection(reseau, polygone)
   sf::st_sf(geometry = reseau_clipped)
 }
